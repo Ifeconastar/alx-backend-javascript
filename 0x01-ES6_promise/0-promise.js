9a725a85b418caf30bea84fa9ff4e32c95a30924
ifeconastar <ifeanyiizuegbu29@gmail.com>
2023-07-01 19:23:33 -0700

    hotfix

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,3 +1,26 @@
-export default function iterateThroughObject(reportWithIterator) {
-  return reportWithIterator.join(' | ');
+function MyAPI() {
+  // Constructor for MyAPI
 }
+
+MyAPI.prototype.getResponseFromAPI = function() {
+  return new Promise((resolve, reject) => {
+    // Simulating an asynchronous API call
+    setTimeout(() => {
+      const response = "Response from API";
+      resolve(response);
+      // In case of an error, you can reject the promise
+      // reject(new Error("API Error"));
+    }, 2000);
+  });
+};
+
+// Usage example
+const myApi = new MyAPI();
+myApi.getResponseFromAPI()
+  .then(response => {
+    console.log(response); // Output: "Response from API"
+  })
+  .catch(error => {
+    console.error(error); // Output: Error message in case of rejection
+  });
+

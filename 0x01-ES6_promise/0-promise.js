5c0b923b0a56e1da641224f82275767f96f84368
ifeconastar <ifeanyiizuegbu29@gmail.com>
2023-07-04 07:42:06 -0700

    hot fix

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,18 +1,4 @@
-function MyAPI() {
-  // Constructor for MyAPI
+/* eslint-disable */
+export default function getResponseFromAPI() {
+	return new Promise((resolve, reject) => {});
 }
-
-MyAPI.prototype.getResponseFromAPI = function() {
-  return new Promise((resolve, reject) => {
-    // Simulate an asynchronous API call
-    setTimeout(() => {
-      const response = "Response from API";
-      resolve(response);
-      // In case of an error, you can reject the promise
-      // reject(new Error("API Error"));
-    }, 2000);
-  });
-};
-
-export default MyAPI;
-
